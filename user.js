e3464dc63efacafc0616faff433c4b9b8340eb5d
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2024-08-08 15:28:21 +0300

    Добавил параметры необходимые для стабильной работы

--- user.js
+++ user.js
@@ -1,9 +1,14 @@
-// Hide tabs toolbar
-user_pref("browser.tabs.tabmanager.enabled", false);
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-
 // DraculaTheme recommendation
-user_pref("svg.context-properties.content.enabled", true);
-user_pref("layout.css.color-mix.enabled", true);
 user_pref("browser.tabs.tabClipWidth", 86);
 user_pref("browser.tabs.tabMinWidth", 66);
+
+// Enables the userChrome.css and userContent.css files.
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+// Allows the theme's SVG icons to be coloured properly.
+user_pref("svg.context-properties.content.enabled", true);
+// Allows colours used in the theme to be mixed with others.
+user_pref("layout.css.color-mix.enabled", true);
+// Allows theme to use different colours for light/dark mode.
+user_pref("layout.css.light-dark.enabled", true);
+// Enables the CSS :has() selector, required for hide tabs toolbar tweak.
+user_pref("layout.css.has-selector.enabled", true);
