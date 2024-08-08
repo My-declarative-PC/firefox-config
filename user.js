79d30c56c182054c6a57bef7e8d8aee4512a580c
Timofey Sitnikov <t.sitnikov@infocom-s.ru>
2024-08-08 15:25:28 +0300

    Перенёс файл конфигов
    и немножка упростил его

---
+++ user.js
@@ -0,0 +1,9 @@
+// Hide tabs toolbar
+user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// DraculaTheme recommendation
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("browser.tabs.tabClipWidth", 86);
+user_pref("browser.tabs.tabMinWidth", 66);
